2ff24b65eb0b25aa78cb91d29a3429555b9ab754
borgeha <borgeha@10.53.49.164>
2020-02-03 12:35:41 +0100

    ferdig på forelesning

--- foreksempel/src/uke6/arv/Barnebok.java
+++ foreksempel/src/uke6/arv/Barnebok.java
@@ -3,15 +3,15 @@ package uke6.arv;
 public class Barnebok extends Bok {
 
 	int maksalder;
-	
+
 	public Barnebok(String tittel, String forfatter, int sideantall, int maksalder) {
 		super(tittel, forfatter, sideantall);
 		this.maksalder = maksalder;
 	}
 
-	
 
-	// Legg merke til at vi kan kalle super sin toString() for å skrive ut de variablene som
+
+	// Legg merke til at vi kan kalle super sin toString() for ï¿½ skrive ut de variablene som
 	// Barnebok har arvet fra Bok! Super kjenner ikke til maksalder.
 	@Override
 	public String toString() {
@@ -20,9 +20,9 @@ public class Barnebok extends Bok {
 
 
 	public static void main(String[] args) {
-		Barnebok bb = new Barnebok("Ruffen", "Bringsværd", 44, 100);
+		Barnebok bb = new Barnebok("Ruffen", "Bringsvï¿½rd", 44, 100);
 		System.out.println(bb);
-		
+
 	}
 
 }
